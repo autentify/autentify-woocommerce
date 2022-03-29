7a1e7a46c60d9d0eb825c4e8b14dd4e0f95116c0
Joao Vitor Barbosa <joaovitorwwd@hotmail.com>
2022-03-29 19:03:05 -0300

    Add to update the meta data after check the AutentiMail.

--- app/actions/autentify_autenti_mail_actions.php
+++ app/actions/autentify_autenti_mail_actions.php
@@ -13,7 +13,8 @@ if ( ! function_exists( 'autentify_autenti_mail_post' ) ) {
       die();
     }
 
-    $response['email'] = sanitize_email( $_REQUEST['param1'] );
+    $order_id = sanitize_text_field( $_REQUEST['param1'] );
+    $response['email'] = sanitize_email( $_REQUEST['param2'] );
 
     if ( ! Autentify_Email_Helper::get_instance()->is_valid_email( $response['email'] ) ) {
       $response['success'] = false;
@@ -25,9 +26,6 @@ if ( ! function_exists( 'autentify_autenti_mail_post' ) ) {
     $autenti_mail_DAO = Autentify_Autenti_Mail_DAO::get_instance();
     $autenti_mail_response = $autenti_mail_DAO->check( $response['email'] );
 
-    echo json_encode( $autenti_mail_response );
-    die();
-
     $response['success'] = $autenti_mail_response->status == '201';
     $response['message'] = '';
     
@@ -35,8 +33,16 @@ if ( ! function_exists( 'autentify_autenti_mail_post' ) ) {
       $response['message'] = Autentify_Api::get_instance()->get_error_messsage( $autenti_mail_response->code );
     } else {
       $response['message'] = 'Consulta finalizada com sucesso!';
-      // GET the order ID
-      // Save the order meta data: external_id, risk_score, risk_score-msg and description
+
+      $order = wc_get_order( $order_id );
+
+      // WooCommerce 3.0 or later.
+      if ( ! method_exists( $order, 'update_meta_data' ) ) {
+        update_post_meta( $order_id, "autenti_mail", $autenti_mail_response->autenti_mail );
+      } else {
+        $order->update_meta_data( "autenti_mail", $autenti_mail_response->autenti_mail );
+        $order->save();
+      }
     }
 
     echo json_encode( $response );
