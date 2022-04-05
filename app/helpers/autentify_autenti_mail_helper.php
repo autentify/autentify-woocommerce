8604c6db3f2c46604dddeae24d3930d20ecc7673
Joao Vitor Barbosa <joaovitorwwd@hotmail.com>
2022-04-04 21:24:06 -0300

    Create Autentify_Autenti_Mail_Helper.

---
+++ app/helpers/autentify_autenti_mail_helper.php
@@ -0,0 +1,32 @@
+<?php
+
+class Autentify_Autenti_Mail_Helper {
+  private static $instance;
+  
+  private function __construct() {}
+
+  public static function get_instance() {
+    if ( ! isset( self::$instance ) ) {
+      self::$instance = new Autentify_Autenti_Mail_Helper();
+    }
+    return self::$instance;
+  }
+
+  /**
+   * Formats the String CPF with special characters into a String
+   * with only numbers and without left zeros.
+   * @param String $cpf The String that contains the CPF to be formatted.
+   * @return String with The formatted cpf.
+   */
+  public function format( $cpf ) {
+    if (! isset( $cpf )) return $cpf;
+
+    $cpf = trim( $cpf );
+    if ( empty( $cpf ) ) return $cpf;
+
+    $cpf = preg_replace( "/[^0-9]/", "", $cpf );
+    $cpf = strval( (int) $cpf );
+    
+    return $cpf;
+  }
+}
